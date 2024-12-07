1be4248b75a9eb50681e340af18eef313b74f03f
dkk <dkkloimwieder@gmail.com>
2024-12-07 16:00:37 -0500

    reinit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 
-import solid from "@astrojs/solid-js";
+import solidJs from '@astrojs/solid-js';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solid()],
-  output: 'server',
-  adapter: cloudflare(),
+  integrations: [solidJs()]
 });
